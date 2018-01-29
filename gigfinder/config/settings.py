6a2d0e17fcbd4c3428037c2354ce9e5ce19c4d4b
casamfiml <kasam@protonmail.com>
2018-01-29 10:22:41 -0500

    Update settings.py

--- gigfinder/config/settings.py
+++ gigfinder/config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&p!bwqd_-v+e(fgr+%uhe3$%v*e21!qn#86avdln6z3u5+59ks'
+SECRET_KEY = [YOUR SECRET KEY]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
